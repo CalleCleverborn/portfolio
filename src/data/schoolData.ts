73f3956af2783b98d3bbaa0015cd69611c972abd
CalleCleverborn <CalleCleverborn@users.noreply.github.com>
2024-03-21 20:47:50 +0000

    deploy: d919a77b7e30cb40770a192bb6b1b21f80320fd9

--- src/data/schoolData.ts
+++
@@ -1,8 +0,0 @@
-import { SchoolItem } from '../models/types'; // Importing the type
-import mediagymnasiet from '../assets/mediagymnasiet.jpg';
-import medieinstitutet from '../assets/medieinstitutet.png';
-
-export const schoolData: SchoolItem[] = [
-    { id: 1, url:mediagymnasiet, description: ' Information and Media Technology', label: 'Mediagymnasiet', time: "2016-2019"},
-    { id: 3, url:medieinstitutet, description: 'E-commerce Web Development', label: 'Medieinstitutet', time: "2023-ongoing"}
-]
--- src/data/selfData.ts
+++
@@ -1,4 +0,0 @@
-import { SelfItem } from "../models/types";
-
-export const selfData: SelfItem = {greeting: "Hello I'm Carl Cleverborn", occupation: "Studying Web-Development at Medieinstitutet", description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est perferendis nihil fuga doloremque sunt eaque accusantium voluptatum nulla eveniet ea, modi culpa molestias velit. Impedit similique consequatur blanditiis explicabo ducimus!"}
-    
